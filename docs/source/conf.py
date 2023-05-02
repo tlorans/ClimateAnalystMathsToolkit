0c30e77924022d7583b9ef34cfbe6bddd7662464
tlorans <thomaslorans@gmail.com>
2023-05-02 14:15:53 +0200

    fixed conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,25 +31,20 @@ release = '0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'myst_parser',
+    'sphinx.ext.autosectionlabel',
+    "nbsphinx",
+    "sphinxcontrib.bibtex",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+bibtex_bibfiles = ['refs.bib']
+myst_enable_extensions = ["dollarmath", "amsmath"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+templates_path = ['_templates']
 exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_theme = 'sphinx_book_theme'
+html_static_path = []
